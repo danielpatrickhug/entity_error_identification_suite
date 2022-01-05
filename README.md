f0dc63542f917c059ec0151994c1e0d5ae1aed60
Daniel Hug <danielpatrickhug@gmail.com>
2022-01-05 12:26:24 -0500

    module organization was a mess, fixed it now.

--- README.md
+++ README.md
@@ -11,6 +11,5 @@
 ```bash
 python3.9 -m venv venv && source venv/bin/activate && pip install -U pip setuptools wheel
 pip install -r requirements.txt
-cd src
 python main.py
 ```
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from spacy_pipeline_config import PipelineConfig
-from data_preprocessor import DataPreProcessor
-from nerror_identifier import ErrorIdentifier
+from src.spacy_pipeline_config import PipelineConfig
+from src.data_preprocessor import DataPreProcessor
+from src.nerror_identifier import ErrorIdentifier
 
 config = PipelineConfig("en_core_web_trf")
 nlp = config.get_nlp()
 vocab = config.get_voab()
-processor = DataPreProcessor("../data/test.json", "index")
+processor = DataPreProcessor("./data/test.json", "index")
 df = processor.get_dataframe()
 labels = ["PERSON", "ORG", "GPE", "LOC", "PRODUCT", "EVENT", "WORK_OF_ART"]
 
