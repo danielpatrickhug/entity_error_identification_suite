3849307eae9903c18423cba18b3af5d908daae41
Daniel Hug <danielpatrickhug@gmail.com>
2022-01-06 10:56:47 -0500

    cleaned up main.py

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ from src.nerror_identifier import ErrorIdentifier
 
 config = PipelineConfig("en_core_web_trf")
 nlp = config.get_nlp()
-vocab = config.get_voab()
 processor = DataPreProcessor("./data/test.json", "index")
 df = processor.get_dataframe()
 labels = ["PERSON", "ORG", "GPE", "LOC", "PRODUCT", "EVENT", "WORK_OF_ART"]
-#stop_words = config.get_stop_words()
-#conj_advs = config.get_conj_advs()
 
 df.apply(lambda x: ErrorIdentifier(nlp(x["text"]), list(x["ground_truth_entities_list"]),
                                    config.get_voab(), labels).identify_errors(), axis=1)
