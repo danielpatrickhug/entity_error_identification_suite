6e1643fdf53730c507cd1b42bcf76802ff355130
Daniel Hug <danielpatrickhug@gmail.com>
2022-01-06 11:28:39 -0500

    changed param name ground_truth_list in SpanMatcher to be the more general entity_list

--- src/span_matcher.py
+++ src/span_matcher.py
@@ -2,13 +2,13 @@ from spacy.tokens import Span
 from spacy.matcher import Matcher
 
 class SpanMatcher:
-    def __init__(self, doc, vocab, ground_truth_list, label):
+    def __init__(self, doc, vocab, entity_list, label):
         self.doc = doc
         self.vocab = vocab
         self.matcher = Matcher(self.vocab)
-        self.ground_truth_list = ground_truth_list
+        self.entity_list = entity_list
         self.label = label
-        for idx, ent in enumerate(self.ground_truth_list): self.match_string_to_doc_obj(idx, ent)
+        for idx, ent in enumerate(self.entity_list): self.match_string_to_doc_obj(idx, ent)
         self.matches = self.matcher(self.doc)
 
     def match_string_to_doc_obj(self, idx: int, ent: str) -> None:
