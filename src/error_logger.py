8339bc7c1cffbc319900a5f4df54063218897935
Daniel Hug <danielpatrickhug@gmail.com>
2022-01-09 14:19:43 -0500

    fragmentation error check broke

--- src/error_logger.py
+++ src/error_logger.py
@@ -10,14 +10,18 @@ class ErrorLogger:
                         ("Shift Right Fragmentation Error", 1, 1, 1), ("Bilateral Fragmentation Error", 1, -1, 1)]
 
     #TODO Log to tsv
-    def log_general(self, prediction_type, doc_ent: Span, ent: Span) -> None:
+    def log_general(self, prediction_type: str, doc_ent: Span, ent: Span) -> None:
         print(f"{prediction_type} \t {self.doc[doc_ent.start:doc_ent.end]} \t {self.doc[ent.start:ent.end]}")
 
-    def is_singleton(self, ent: Span) -> bool:
-        return len(ent) == 1
+    def is_not_singleton(self, ent: Span) -> bool:
+        return len(ent) != 1
 
-    def check_entity_pair(self, doc_ent: Span, ent: Span, start_offset, end_offset) -> bool:
-        return doc_ent.start == ent.start+start_offset and doc_ent.end == ent.end+end_offset
+    def check_entity_pair(self, doc_ent: Span, ent: Span, start_offset, end_offset, singleton_flag) -> bool:
+        if singleton_flag:
+            print(f"{doc_ent.text} \t {ent.text}")
+            return doc_ent.start == ent.start+start_offset and doc_ent.end == ent.end+end_offset and self.is_not_singleton(doc_ent)
+        else:
+            return doc_ent.start == ent.start+start_offset and doc_ent.end == ent.end+end_offset
 
     def get_overlapping_entities(self, ent: Span) -> list:
         return [doc_ent for doc_ent in self.doc.ents if doc_ent.end>= ent.start and doc_ent.start<= ent.end]
@@ -29,16 +33,10 @@ class ErrorLogger:
         for idx, ent in enumerate(ground_truth_spans):
             for doc_ent in self.get_overlapping_entities(ent):
                 for error_type in self.error_types:
-                    if error_type[3] == 1:
-                        if self.check_entity_pair(doc_ent, ent, error_type[1], error_type[2]) and self.is_singleton(doc_ent):
-                            self.log_general(error_type[0], doc_ent, ent)
-                            break_loop = True
-                            break
-                    else:
-                        if self.check_entity_pair(doc_ent, ent, error_type[1], error_type[2]):
-                            self.log_general(error_type[0], doc_ent, ent)
-                            break_loop = True
-                            break
+                    if self.check_entity_pair(doc_ent, ent, error_type[1], error_type[2], error_type[3]):
+                        self.log_general(error_type[0], doc_ent, ent)
+                        break_loop = True
+                        break
             if break_loop:
                 break_loop = False
                 continue
